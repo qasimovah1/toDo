a846128c6a0c5fa9b971df063e6a2b3a7e4590ec
humay qasimova <hqasimova472@gmail.com>
2024-03-20 19:17:48 +0000

    todo

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 "use strict";
 $(document).ready(function () {
-
   $('.btn').click (function (e) {
     e.preventDefault();
-    if ()
+    if (!$.trim($('input').val()) == '') {
+      let x = y =>{
+        for (let i = 0 ; i < y ; i++) {
+          $('.box').append(`<h2>${y}</h2>`);
+        }
+      };
+      let num = h => {
+        if (Number(h)){
+          return x(h);
+        }
+        else{
+          $('.box').append(`<h2>${h}</h2>`);
+        }
+      };
+      num ($('input').val());
+    }
+    $('input').val('');
   })
 });
